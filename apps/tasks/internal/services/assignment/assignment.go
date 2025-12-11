a9cb2d989c6835fae4e35ead92cca00a7fa6d1ad
Daniil Emelin <kaptoshka4@gmail.com>
2025-12-12 02:49:24 +0400

    feat: added assignment service init func

--- apps/tasks/internal/services/assignment/assignment.go
+++ apps/tasks/internal/services/assignment/assignment.go
@@ -21,3 +21,22 @@ type AssignmentProvider interface {
 	AssignmentByID(ctx context.Context, id int64) (*models.Assignment, error)
 	AssignmentByTeacherID(ctx context.Context, teacherID int64) ([]*models.Assignment, error)
 }
+
+func New(
+	log *slog.Logger,
+	assignmentProvider AssignmentProvider,
+	assignmentSaver AssignmentSaver,
+) *AssignmentService {
+	return &AssignmentService{
+		log:                log,
+		assignmentProvider: assignmentProvider,
+		assignmentSaver:    assignmentSaver,
+	}
+}
+
+func (s *AssignmentService) Assignment(
+	ctx context.Context,
+	assignmentID int64,
+) (models.Assignment, models.Submission, error) {
+
+}
