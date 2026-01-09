c97e18d51f4a3efe6dbde8515ff6fdce8fc0997f
Daniil Emelin <kaptoshka4@gmail.com>
2026-01-09 04:12:55 +0400

    refactor: change logic models of database, update proto files for tasks

---
+++ apps/tasks/migrations/001_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS submissions;
+DROP TABLE IF EXISTS submission_attempts;
+DROP TABLE IF EXISTS feedbacks;
+DROP TABLE IF EXISTS widgets;
--- apps/tasks/migrations/001_init.up.sql
+++ apps/tasks/migrations/001_init.up.sql
@@ -1,28 +1,50 @@
 CREATE TABLE IF NOT EXISTS assignments (
-	id SERIAL PRIMARY KEY,
-	title VARCHAR(255) NOT NULL,
-	content JSONB NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	deadline TIMESTAMP NOT NULL,
-	teacher_id INTEGER NOT NULL
+    id UUIDv7 PRIMARY KEY,
+    creator_id UUIDv7 NOT NULL,
+    student_id UUIDv7 NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    widget_id INTEGER NOT NULL REFERENCES widgets(id),
+    widget_config JSONB NOT NULL,
+    due_date TIMESTAMP NOT NULL,
+    cutoff_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS widgets(
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255) NOT NULL,
+    version INTEGER NOT NULL,
+    config_schema JSONB NOT NULL,
+    submission_schema JSONB NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS submissions (
-	id SERIAL PRIMARY KEY,
-	assignment_id integer NOT NULL,
-	student_id INTEGER NOT NULL,
-	content JSONB NOT NULL,
-	started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	status VARCHAR(255) NOT NULL,
-	feedback VARCHAR(255),
-	FOREIGN KEY (assignment_id) REFERENCES assignments(id),
+    id UUIDv7 PRIMARY KEY,
+    assignment_id UUIDv7 NOT NULL REFERENCES assignments(id),
+    creator_id UUIDv7 NOT NULL,
+    status VARCHAR(60) NOT NULL,
+    current_version_id UUIDv7 NOT NULL REFERENCES submission_versions(id),
+    started_at TIMESTAMP NOT NULL,
+    submitted_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS assignment_targets (
-    id SERIAL PRIMARY KEY,
-    assignment_id INTEGER NOT NULL,
-    student_id INTEGER NOT NULL,
-    FOREIGN KEY (assignment_id) REFERENCES assignments(id),
+CREATE TABLE IF NOT EXISTS submission_versions (
+    id UUIDv7 PRIMARY KEY,
+    submission_id UUIDv7 NOT NULL REFERENCES submissions(id),
+    version_number INTEGER NOT NULL,
+    payload JSONB NOT NULL,
+    is_late BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS feedbacks (
+    id UUIDv7 PRIMARY KEY,
+    submission_version_id UUIDv7 NOT NULL REFERENCES submission_attempts(id),
+    grader_id UUIDv7 NOT NULL,
+    feedback VARCHAR(255) NOT NULL,
+    is_published BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
