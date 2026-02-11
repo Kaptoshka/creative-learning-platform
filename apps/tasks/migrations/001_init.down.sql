bc8f7167a942b6003da1c3182d043b56b3ff3ab0
Daniil Emelin <kaptoshka4@gmail.com>
2026-02-12 01:01:58 +0400

    refactor: change logic models of database

--- apps/tasks/migrations/001_init.down.sql
+++ apps/tasks/migrations/001_init.down.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS assignment_templates;
-DROP TABLE IF EXISTS student_assignments;
 DROP TABLE IF EXISTS widgets;
+DROP TABLE IF EXISTS assignment_templates;
+DROP TABLE IF EXISTS assignment_targets;
 DROP TABLE IF EXISTS submissions;
 DROP TABLE IF EXISTS submission_versions;
 DROP TABLE IF EXISTS feedbacks;
--- apps/tasks/migrations/001_init.up.sql
+++ apps/tasks/migrations/001_init.up.sql
@@ -1,57 +1,81 @@
-CREATE TABLE IF NOT EXISTS assignment_templates (
-    id UUID PRIMARY KEY,
-    creator_id UUID NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    widget_id INTEGER NOT NULL REFERENCES widgets(id),
-    widget_config JSONB NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS student_assignments (
-    id UUID PRIMARY KEY,
-    template_id UUID NOT NULL REFERENCES assignment_templates(id),
-    student_id UUID NOT NULL,
-    due_date TIMESTAMP NOT NULL,
-    cutoff_date TIMESTAMP NOT NULL,
-    status VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS widgets(
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+
     type VARCHAR(255) NOT NULL,
     version INTEGER NOT NULL,
-    config_schema JSONB NOT NULL,
-    submission_schema JSONB NOT NULL
+
+    config_schema JSONB NOT NULL DEFAULT '{}',
+    submission_schema JSONB NOT NULL DEFAULT '{}',
+    UNIQUE (type, version)
+);
+
+CREATE TABLE IF NOT EXISTS assignment_templates (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    creator_id UUID NOT NULL, -- Teacher ID
+
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+
+    widget_id UUID NOT NULL REFERENCES widgets(id),
+    widget_config JSONB NOT NULL DEFAULT '{}',
+
+    due_date TIMESTAMP NOT NULL,    -- Soft deadline
+    cutoff_date TIMESTAMP NOT NULL, -- Hard deadline
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS assignment_targets (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    template_id UUID NOT NULL REFERENCES assignment_templates(id),
+
+    group_id UUID,
+    student_id UUID,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT check_target_not_empty CHECK (group_id IS NOT NULL OR student_id IS NOT NULL)
 );
 
 CREATE TABLE IF NOT EXISTS submissions (
-    id UUID PRIMARY KEY,
-    assignment_id UUID NOT NULL REFERENCES student_assignments(id),
-    creator_id UUID NOT NULL,
-    status VARCHAR(60) NOT NULL,
-    current_version_id UUID REFERENCES submission_versions(id),
-    started_at TIMESTAMP NOT NULL,
-    submitted_at TIMESTAMP
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+
+    template_id UUID NOT NULL REFERENCES assignment_templates(id),
+    student_id UUID NOT NULL,
+
+    status VARCHAR(60) NOT NULL DEFAULT 'IN_PROGRESS',
+    -- 'IN_PROGRESS', 'SUBMITTED', 'GRADED', 'RETURNED'
+
+    started_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    submitted_at TIMESTAMP,
+
+    CONSTRAINT uniq_student_template UNIQUE (student_id, template_id)
 );
+CREATE INDEX idx_submissions_template_status ON submissions (template_id, status);
+CREATE INDEX idx_submissions_student ON submissions (student_id);
 
 CREATE TABLE IF NOT EXISTS submission_versions (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
     submission_id UUID NOT NULL REFERENCES submissions(id),
     version_number INTEGER NOT NULL,
     payload JSONB NOT NULL,
-    is_late BOOLEAN NOT NULL,
+
+    time_spent_seconds INTEGER NOT NULL DEFAULT 0,
+
+    is_autosave BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE INDEX idx_submission_versions_latest ON submission_versions (submission_id, version_number DESC);
 
 CREATE TABLE IF NOT EXISTS feedbacks (
-    id UUID PRIMARY KEY,
-    submission_version_id UUID NOT NULL REFERENCES submission_versions(id),
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    version_id UUID NOT NULL REFERENCES submission_versions(id),
     grader_id UUID NOT NULL,
-    feedback TEXT NOT NULL,
+    text_content TEXT,
+    payload JSONB,
     is_published BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
