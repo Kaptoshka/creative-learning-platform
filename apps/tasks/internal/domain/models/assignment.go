30351ff6e03e997a34d140099987f2a9a676dccc
Kaptoshka <kaptoshka@duck.com>
2026-04-17 19:37:22 +0400

    feat(models): add dto for target, createAssignment and feedback

--- apps/tasks/internal/domain/models/assignment.go
+++ apps/tasks/internal/domain/models/assignment.go
@@ -7,15 +7,15 @@ import (
 )
 
 type AssignmentTemplate struct {
-	ID           uuid.UUID `db:"id"`
-	CreatorID    uuid.UUID `db:"creator_id"`
-	Title        string    `db:"title"`
-	Description  string    `db:"description"`
-	WidgetID     uuid.UUID `db:"widget_id"`
-	WidgetConfig JSONB     `db:"widget_config"`
-	DueDate      time.Time `db:"due_date"`
-	CreatedAt    time.Time `db:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at"`
+	ID           uuid.UUID  `db:"id"`
+	CreatorID    uuid.UUID  `db:"creator_id"`
+	Title        string     `db:"title"`
+	Description  string     `db:"description"`
+	WidgetID     uuid.UUID  `db:"widget_id"`
+	WidgetConfig JSONB      `db:"widget_config"`
+	DueDate      *time.Time `db:"due_date"`
+	CreatedAt    time.Time  `db:"created_at"`
+	UpdatedAt    time.Time  `db:"updated_at"`
 }
 
 type AssignmentTemplateLight struct {
@@ -23,7 +23,7 @@ type AssignmentTemplateLight struct {
 	Title      string           `db:"title"`
 	Status     SubmissionStatus `db:"status"`
 	WidgetType string           `db:"widget_type"`
-	DueDate    time.Time        `db:"due_date"`
+	DueDate    *time.Time       `db:"due_date"`
 }
 
 type AssignmentTarget struct {
@@ -33,4 +33,19 @@ type AssignmentTarget struct {
 	StudentID  *uuid.UUID `db:"student_id"`
 	CreatedAt  time.Time  `db:"created_at"`
 	UpdatedAt  time.Time  `db:"updated_at"`
+}
+
+type TargetDTO struct {
+	TemplateID uuid.UUID  `db:"template_id"`
+	GroupID    *uuid.UUID `db:"group_id"`
+	StudentID  *uuid.UUID `db:"student_id"`
+}
+
+type CreateAssignmentDTO struct {
+	Title        string       `db:"title"`
+	Description  string       `db:"description"`
+	WidgetID     uuid.UUID    `db:"widget_id"`
+	WidgetConfig JSONB        `db:"widget_config"`
+	DueDate      *time.Time   `db:"due_date"`
+	Targets      []*TargetDTO `db:"targets"`
 }
--- apps/tasks/internal/domain/models/feedback.go
+++ apps/tasks/internal/domain/models/feedback.go
@@ -16,3 +16,11 @@ type Feedback struct {
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
 }
+
+type FeedbackDTO struct {
+	VersionID    uuid.UUID `db:"version_id"`
+	SubmissionID uuid.UUID `db:"submission_id"`
+	TextContent  string    `db:"text_content"`
+	Payload      JSONB     `db:"payload"`
+	IsPublished  bool      `db:"is_published"`
+}
