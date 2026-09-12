ed879549b39bccc4892adc744407b84a415f6837
Kaptoshka <kaptoshka@duck.com>
2026-09-12 20:26:32 +0400

    style: lint vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,59 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
-export default defineConfig({
-    root: "src",
-    build: {
-        rollupOptions: {
-            input: "./src/index.html",
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        root: "src",
+        build: {
+            outDir: "../dist",
+            emptyOutDir: true,
+            rollupOptions: {
+                input: "./index.html",
+            },
         },
-    },
-    server: {
-        host: true,
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+        server: {
+            host: true,
+            port: 3000,
         },
-    },
-    define: {
-        "process.env.NODE_ENV": JSON.stringify(
-            process.env.NODE_ENV || "development",
-        ),
-        "process.env.BUN_PUBLIC_SSO_API_URL": JSON.stringify(
-            process.env.BUN_PUBLIC_SSO_API_URL || "http://localhost:8080",
-        ),
-        "process.env.BUN_PUBLIC_APP_ID": JSON.stringify(
-            process.env.BUN_PUBLIC_APP_ID || "1",
-        ),
-    },
-    css: {
-        modules: {
-            localsConvention: "camelCase",
+        proxy: {
+            "/api/v1": {
+                target: "http://gateway-service:8000/api/v1",
+                changeOrigin: true,
+            },
         },
-    },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+        define: {
+            "import.meta.env.VITE_API_URL": JSON.stringify(
+                env.VITE_API_URL || "/",
+            ),
+            "import.meta.env.VITE_APP_ID": JSON.stringify(
+                env.VITE_APP_ID || "1",
+            ),
+            "process.env.NODE_ENV": JSON.stringify(
+                process.env.NODE_ENV || "development",
+            ),
+            "process.env.BUN_PUBLIC_SSO_API_URL": JSON.stringify(
+                process.env.BUN_PUBLIC_SSO_API_URL || "http://localhost:8080",
+            ),
+            "process.env.BUN_PUBLIC_APP_ID": JSON.stringify(
+                process.env.BUN_PUBLIC_APP_ID || "1",
+            ),
+        },
+        css: {
+            modules: {
+                localsConvention: "camelCase",
+            },
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler",
+                },
+            },
+        },
+    };
 });
